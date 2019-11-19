dc1ecaafadd7184ba4e12217ae2faf5654edb3b6
pradeep71d <pkdubey071@gmail.com>
2019-11-19 12:36:18 +0530

    add

---
+++ app/src/main/java/com/example/newdemotext/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.newdemotext;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        int arr[] = {860, 8, 200, 9};
+        System.out.println("before Sorting arra is:=");
+        int temp = 0;
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(" " + arr[i]);
+        }
+        //swapping array here after successful comparison
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        System.out.println();
+        System.out.println("Affter Sorting:::Array is equal:=");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(" " + arr[i]);
+        }
+    }
+}
+
